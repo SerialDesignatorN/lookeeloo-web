1e6fcdb1c8143d101271ea26374691dcba1b08f5
kaitoujokercodes <58360911+kaitoujokercodes@users.noreply.github.com>
2021-04-06 07:25:51 +0700

    Update script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+var txt;
+
+function upload()
+{
+    if(confirm("Please submit the videos to the developer\nA must:\n1. Video must not exceed over 90MB due to GitHub's upload limit\n2. Video must include poster and video.\n3. No inappropiate videos. Curse words are allowed.\n4. The video must not contain racism. \nAny inappropiate content should be reported to the devs.\nPlease submit the video by clicking OK."))
+    {
+        window.location.replace("mailto:zeanfender11@gmail.com");
+    }
+    else
+    {
+        txt = "Cancelled!;"
+    }
+
+}
