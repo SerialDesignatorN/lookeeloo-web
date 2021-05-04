2e33b5085a00df86dd3cc36e5a488fcd695a899f
zeankun.dev <58360911+zeankundev@users.noreply.github.com>
2021-05-04 16:11:03 +0700

    Added my childhood shows on Lookeeloo

---
+++ video/6381937-series/script.js
@@ -0,0 +1,35 @@
+var txt;
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+function upload()
+{
+    if(confirm("Please submit the videos to the developer\nA must:\n1. Video must not exceed over 90MB due to GitHub's upload limit\n2. Video must include poster and video.\n3. No inappropiate videos. Curse words are allowed.\n4. The video must not contain racism. \nAny inappropiate content should be reported to the devs.\nPlease submit the video by clicking OK."))
+    {
+        window.location.replace("mailto:zeanfender11@gmail.com");
+    }
+    else
+    {
+        txt = "Cancelled!;"
+    }
+
+}
+
+
+function() {
+    var dialog = document.getElementById('window');
+  document.getElementById('show').addEventListener("click", function() 
+  {
+    
+  }
+}
+function myFunction() 
+{
+    //your codes here Zean
+}
