e39855ff6b73be7e4c0741a8497984bfde7ee5af
zeankun.dev <58360911+zeankundev@users.noreply.github.com>
2021-05-11 12:15:00 +0700

    Add files via upload

--- video/6381937-series/script.js
+++ video/6381937-series/script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var txt;
 // Get the modal
 var modal = document.getElementById("myModal");
@@ -32,4 +33,40 @@ function() {
 function myFunction() 
 {
     //your codes here Zean
+=======
+var txt;
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+function upload()
+{
+    if(confirm("Please submit the videos to the developer\nA must:\n1. Video must not exceed over 90MB due to GitHub's upload limit\n2. Video must include poster and video.\n3. No inappropiate videos. Curse words are allowed.\n4. The video must not contain racism. \nAny inappropiate content should be reported to the devs.\nPlease submit the video by clicking OK."))
+    {
+        window.location.replace("mailto:zeanfender11@gmail.com");
+    }
+    else
+    {
+        txt = "Cancelled!;"
+    }
+
+}
+
+
+function() {
+    var dialog = document.getElementById('window');
+  document.getElementById('show').addEventListener("click", function() 
+  {
+    
+  }
+}
+function myFunction() 
+{
+    //your codes here Zean
+>>>>>>> bb137ce78ffada58f0c80b9494bd0c7074795b6e
 }
