1f149ef0089ca5fd25d0890690895f8b910a2724
zeankun.dev <58360911+zeankundev@users.noreply.github.com>
2021-05-11 12:02:03 +0700

    Add files via upload

--- video/6381937-series/vid/e1/player-actions.js
+++ video/6381937-series/vid/e1/player-actions.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const video = document.getElementById('video');
 const videoControls = document.getElementById('video-controls');
 const playbackAnimation = document.getElementById('playback-animation');
@@ -243,3 +244,250 @@ function keyboardShortcuts(event) {
     }
   }
   document.addEventListener('keyup', keyboardShortcuts);
+=======
+const video = document.getElementById('video');
+const videoControls = document.getElementById('video-controls');
+const playbackAnimation = document.getElementById('playback-animation');
+const videoWorks = !!document.createElement('video').canPlayType;
+const playButton = document.getElementById('play');
+const playbackIcons = document.querySelectorAll('.playback-icons use');
+const timeElapsed = document.getElementById('time-elapsed');
+const duration = document.getElementById('duration');
+const progressBar = document.getElementById('progress-bar');
+const seek = document.getElementById('seek');
+const volumeButton = document.getElementById('volume-button');
+const volumeIcons = document.querySelectorAll('.volume-button use');
+const volumeMute = document.querySelector('use[href="#volume-mute"]');
+const volumeLow = document.querySelector('use[href="#volume-low"]');
+const volumeHigh = document.querySelector('use[href="#volume-high"]');
+const volume = document.getElementById('volume');
+const seekTooltip = document.getElementById('seek-tooltip');
+const fullscreenButton = document.getElementById('fullscreen-button');
+const videoContainer = document.getElementById('video-container');
+const pipButton = document.getElementById('pip-button')
+
+
+
+if (videoWorks) {
+  video.controls = false;
+  videoControls.classList.remove('hidden');
+}
+
+function togglePlay() {
+    if (video.paused || video.ended) {
+      video.play();
+    } else {
+      video.pause();
+    }
+  }
+  playButton.addEventListener('click', togglePlay);
+  
+  function updatePlayButton() {
+    playbackIcons.forEach(icon => icon.classList.toggle('hidden'));
+  }
+  video.addEventListener('play', updatePlayButton);
+video.addEventListener('pause', updatePlayButton);
+function updatePlayButton() {
+    playbackIcons.forEach(icon => icon.classList.toggle('hidden'));
+  
+    if (video.paused) {
+      playButton.setAttribute('data-title', 'Play (k)')
+    } else {
+      playButton.setAttribute('data-title', 'Pause (k)')
+    }
+  }
+
+function formatTime(timeInSeconds) {
+    const result = new Date(timeInSeconds * 1000).toISOString().substr(11, 8);
+  
+    return {
+      minutes: result.substr(3, 2),
+      seconds: result.substr(6, 2),
+    };
+  };
+  function initializeVideo() {
+    const videoDuration = Math.round(video.duration);
+    const time = formatTime(videoDuration);
+    duration.innerText = `${time.minutes}:${time.seconds}`;
+    duration.setAttribute('datetime', `${time.minutes}m ${time.seconds}s`)
+  }
+  video.addEventListener('loadedmetadata', initializeVideo);
+  function updateTimeElapsed() {
+    const time = formatTime(Math.round(video.currentTime));
+    timeElapsed.innerText = `${time.minutes}:${time.seconds}`;
+    timeElapsed.setAttribute('datetime', `${time.minutes}m ${time.seconds}s`)
+  }
+  video.addEventListener('timeupdate', updateTimeElapsed);
+  function initializeVideo() {
+    const videoDuration = Math.round(video.duration);
+    seek.setAttribute('max', videoDuration);
+    progressBar.setAttribute('max', videoDuration);
+    const time = formatTime(videoDuration);
+    duration.innerText = `${time.minutes}:${time.seconds}`;
+    duration.setAttribute('datetime', `${time.minutes}m ${time.seconds}s`)
+  }
+  function updateProgress() {
+    seek.value = Math.floor(video.currentTime);
+    progressBar.value = Math.floor(video.currentTime);
+  }
+  video.addEventListener('timeupdate', updateProgress);
+  
+  function updateSeekTooltip(event) {
+    const skipTo = Math.round((event.offsetX / event.target.clientWidth) * parseInt(event.target.getAttribute('max'), 10));
+    seek.setAttribute('data-seek', skipTo)
+    const t = formatTime(skipTo);
+    seekTooltip.textContent = `${t.minutes}:${t.seconds}`;
+    const rect = video.getBoundingClientRect();
+    seekTooltip.style.left = `${event.pageX - rect.left}px`;
+  }
+  seek.addEventListener('mousemove', updateSeekTooltip);
+  function skipAhead(event) {
+    const skipTo = event.target.dataset.seek ? event.target.dataset.seek : event.target.value;
+    video.currentTime = skipTo;
+    progressBar.value = skipTo;
+    seek.value = skipTo;
+  }
+  seek.addEventListener('input', skipAhead);
+
+  function updateVolume() {
+    if (video.muted) {
+      video.muted = false;
+    }
+  
+    video.volume = volume.value;
+  }
+  volume.addEventListener('input', updateVolume);
+  function updateVolumeIcon() {
+    volumeIcons.forEach(icon => {
+      icon.classList.add('hidden');
+    });
+  
+    volumeButton.setAttribute('data-title', 'Mute (m)')
+  
+    if (video.muted || video.volume === 0) {
+      volumeMute.classList.remove('hidden');
+      volumeButton.setAttribute('data-title', 'Unmute (m)')
+    } else if (video.volume > 0 && video.volume <= 0.5) {
+      volumeLow.classList.remove('hidden');
+    } else {
+      volumeHigh.classList.remove('hidden');
+    }
+  }
+  video.addEventListener('volumechange', updateVolumeIcon);
+  function toggleMute() {
+    video.muted = !video.muted;
+  
+    if (video.muted) {
+      volume.setAttribute('data-volume', volume.value);
+      volume.value = 0;
+    } else {
+      volume.value = volume.dataset.volume;
+    }
+  }
+  volumeButton.addEventListener('click', toggleMute);
+  video.addEventListener('click', togglePlay);
+
+function animatePlayback() {
+    playbackAnimation.animate([
+      {
+        opacity: 1,
+        transform: "scale(1)",
+      },
+      {
+        opacity: 0,
+        transform: "scale(1.3)",
+      }], {
+      duration: 500,
+    });
+  }
+  video.addEventListener('click', animatePlayback);
+
+  function toggleFullScreen() {
+    if (document.fullscreenElement) {
+      document.exitFullscreen();
+    } else if (document.webkitFullscreenElement) {
+      // Need this to support Safari
+      document.webkitExitFullscreen();
+    } else if (videoContainer.webkitRequestFullscreen) {
+      // Need this to support Safari
+      videoContainer.webkitRequestFullscreen();
+    } else {
+      videoContainer.requestFullscreen();
+    }
+  }
+  fullscreenButton.onclick = toggleFullScreen;
+  const fullscreenIcons = fullscreenButton.querySelectorAll('use');
+  function updateFullscreenButton() {
+    fullscreenIcons.forEach(icon => icon.classList.toggle('hidden'));
+  
+    if (document.fullscreenElement) {
+      fullscreenButton.setAttribute('data-title', 'Exit full screen (f)')
+    } else {
+      fullscreenButton.setAttribute('data-title', 'Full screen (f)')
+    }
+  }
+  videoContainer.addEventListener('fullscreenchange', updateFullscreenButton);
+  document.addEventListener('DOMContentLoaded', () => {
+    if (!('pictureInPictureEnabled' in document)) {
+      pipButton.classList.add('hidden');
+    }
+  });
+  
+  async function togglePip() {
+    try {
+      if (video !== document.pictureInPictureElement) {
+        pipButton.disabled = true;
+        await video.requestPictureInPicture();
+      } else {
+        await document.exitPictureInPicture();
+      }
+    } catch (error) {
+      console.error(error)
+    } finally {
+      pipButton.disabled = false;
+    }
+  }
+  pipButton.addEventListener('click', togglePip);
+  function hideControls() {
+    if (video.paused) {
+      return;
+    }
+  
+    videoControls.classList.add('hide');
+  }
+  
+  // showControls displays the video controls
+  function showControls() {
+    videoControls.classList.remove('hide');
+  }
+  video.addEventListener('mouseenter', showControls);
+video.addEventListener('mouseleave', hideControls);
+videoControls.addEventListener('mouseenter', showControls);
+videoControls.addEventListener('mouseleave', hideControls);
+function keyboardShortcuts(event) {
+    const { key } = event;
+    switch(key) {
+      case 'k':
+        togglePlay();
+        animatePlayback();
+        if (video.paused) {
+          showControls();
+        } else {
+          setTimeout(() => {
+            hideControls();
+          }, 2000);
+        }
+        break;
+      case 'm':
+        toggleMute();
+        break;
+      case 'f':
+        toggleFullScreen();
+        break;
+      case 'p':
+        togglePip();
+        break;
+    }
+  }
+  document.addEventListener('keyup', keyboardShortcuts);
+>>>>>>> bb137ce78ffada58f0c80b9494bd0c7074795b6e
